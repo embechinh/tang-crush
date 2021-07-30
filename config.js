2409b8ae27c202e229950eed5b0daa621f498967
EMLACUACAU <88187682+embechinh@users.noreply.github.com>
2021-07-30 16:52:06 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ const CONFIG = {
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, cậu gì à :3',
+    btnYes: 'Vẫn cứ là thích em <33',
+    btnNo: 'Không, em gì à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao cậu lại yêu em <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'Bỡi zì anh iu em gấc nhìuu <33333333',
     mess: 'Em biết mà 🥰. Yêu anh nhiều nhiều 😘😘',
-    messDesc: 'Mai anh ship 🍊🍑🍓cho em bé nhó.',
+    messDesc: 'Mai ship 🍊🍑🍓cho em bé nhó.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
